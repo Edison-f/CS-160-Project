f8cb921634bc8900da75e592fa0dac82564309a1
Edison-f <osgoodbacon@gmail.com>
2025-10-25 15:18:09 -0700

    Update gitignore, add placeholders for item_search and categories, read .env for env vars

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 import os
+from dotenv import load_dotenv
+from api.categories import categories
+from api.item_search import item_search
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -9,5 +14,13 @@ CORS(app)
 def index():
     return send_from_directory('../frontend', 'index.html')
 
+@app.route('/api/categories')
+def api_categories():
+    return categories()
+
+@app.route('/api/item_search')
+def api_item_search():
+    return item_search()
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=int(os.getenv('PORT', 5000)))
